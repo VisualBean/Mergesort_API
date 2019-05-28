afada53d1a0d20023ff5840e95c47876c52bc39d
Alex <alekcarlsen@gmail.com>
2019-05-28 21:33:34 +0200

    add initial execution interfaces and model

---
+++ Mergesort_API/Execution.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="Execution.cs" company="Alexander Steinhauer-Wichmann">
+// Copyright (c) Alexander Steinhauer-Wichmann. All rights reserved.
+// </copyright>
+
+namespace Mergesort_API
+{
+    using System;
+
+    public class Execution
+    {
+        public int Id { get; set; }
+
+        public DateTimeOffset Timestamp { get; set; }
+
+        public int Duration { get; set; }
+
+        public Status Status { get; set; }
+
+        public int[] Input { get; set; }
+
+        public int[] Output { get; set; }
+    }
+
+    public enum Status
+    {
+        Pending,
+        Completed
+    }
+}
