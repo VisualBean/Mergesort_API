5e16be69565ed20f865bf9d97efad890f7a94718
Alex <alekcarlsen@gmail.com>
2019-05-29 01:03:57 +0200

    hookup controller with store and jobrunner

--- Mergesort_API/Controllers/MergesortController.cs
+++ Mergesort_API/Controllers/MergesortController.cs
@@ -4,7 +4,9 @@
 
 namespace Mergesort_API.Controllers
 {
+    using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
 
@@ -12,11 +14,31 @@ namespace Mergesort_API.Controllers
     [ApiController]
     public class MergesortController : ControllerBase
     {
-        // GET api/values
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        private static readonly ISorter<int> Sorter = new MergeSorter();
+        private readonly IJobRunner runner;
+        private readonly IStorageProvider<Guid, SortingJob> jobStore;
+
+        public MergesortController(IJobRunner runner, IStorageProvider<Guid, SortingJob> jobStore)
         {
-            return Ok(new string[] { "value1", "value2" });
+            this.runner = runner;
+            this.jobStore = jobStore;
+        }
+
+        /// <summary>
+        /// Mergesort sorting an array.
+        /// </summary>
+        /// <param name="numbers">The numbers.</param>
+        [HttpPost]
+        [ProducesResponseType(typeof(Job), 201)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> SortArray([FromBody] int[] numbers)
+        {
+            var job = new SortingJob(Sorter, numbers);
+
+            await this.jobStore.Store(job.Id, job);
+            this.runner.Execute(job);
+
+            return this.Accepted(new { job.Id, job.Timestamp, job.Status });
         }
 
         /// <summary>
@@ -27,12 +49,17 @@ namespace Mergesort_API.Controllers
         /// <response code="400">Bad id.</response>
         /// <response code="404">Execution not found.</response>
         [HttpGet("Executions/{id}")]
-        [ProducesResponseType(typeof(Execution),200)]
-        [ProducesResponseType(400)]
+        [ProducesResponseType(typeof(SortingJob), 200)]
         [ProducesResponseType(404)]
-        public async Task<IActionResult> GetExecutionsById(int id)
+        public async Task<IActionResult> GetExecutionsById(Guid id)
         {
-            return Ok(new string[] { "value1", "value2" });
+            var job = await this.jobStore.Retreive(id);
+            if (job == null)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(job);
         }
 
         /// <summary>
@@ -41,11 +68,17 @@ namespace Mergesort_API.Controllers
         /// <response code="200">Executions found.</response>
         /// <response code="404">No executions found.</response>
         [HttpGet("Executions")]
-        [ProducesResponseType(typeof(IEnumerable<Execution>), 200)]
+        [ProducesResponseType(typeof(IEnumerable<Job>), 200)]
         [ProducesResponseType(404)]
         public async Task<IActionResult> GetExecutions()
         {
-            return Ok(new string[] { "value1", "value2" });
+            var jobs = await this.jobStore.GetAll();
+            if (jobs.Any())
+            {
+                 return this.Ok(jobs.Select(job => new { job.Id, job.Timestamp, job.Status }));
+            }
+
+            return this.NotFound();
         }
 
     }
