c641234bd14f13ea0064b84d442f8b0f75255511
Alex <alekcarlsen@gmail.com>
2019-05-28 20:52:34 +0200

    initial api structure

---
+++ Mergesort_API/Controllers/MergesortController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mergesort_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MergesortController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        [HttpGet("Executions/{id}")]
+        public async Task<IActionResult> GetExecutionsById(int id)
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        [HttpGet("Executions")]
+        public async Task<IActionResult> GetExecutions()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+    }
+}
