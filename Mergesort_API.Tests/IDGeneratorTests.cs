701df6a690e70f7eccaf820138dbc0c24483cf66
Alex <alekcarlsen@gmail.com>
2019-05-31 12:40:09 +0200

    added best effort ID generator threading test

--- Mergesort_API.Tests/IDGeneratorTests.cs
+++ Mergesort_API.Tests/IDGeneratorTests.cs
@@ -2,24 +2,44 @@ using FluentAssertions;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Mergesort_API.Tests
 {
     public class IDGeneratorTests
     {
+        [Fact]
+        public async Task GenerateNewId_IncrementsId()
+        {
+            // Best effort thread test for id generation.
+
+            var tasks = new List<Task<int>>();
+            Parallel.For(0, 100, (i) => {
+                tasks.Add(Task.Run(() => IDGenerator.GenerateNewId()));
+            });
+
+            await Task.WhenAll(tasks);
+
+            var numbers = tasks.Select(t => t.Result);
+
+            numbers.Should().OnlyHaveUniqueItems();
+            numbers.Should().NotBeEmpty();
+            numbers.Should().HaveCount(100);
+
+        }
 
         [Fact]
-        public void GenerateNewId_IncrementsId()
+        public void GenerateNewID_IncrementsId()
         {
             var first = IDGenerator.GenerateNewId();
             var second = IDGenerator.GenerateNewId();
 
             first.Should().BeGreaterThan(0);
             first.Should().BeLessThan(second);
-
         }
-
     }
 }
