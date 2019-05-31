d1d70e4b5cb65c2e1df4ade2d370aa6902770c13
Alex <alekcarlsen@gmail.com>
2019-05-31 15:25:00 +0200

    remove unneeded command

--- build.ps1
+++ build.ps1
@@ -1,3 +1,2 @@
-dotnet build /nowarn:MSB3277,SA1600,CS1591,CS4014,CS1998
-dotnet test --no-build
+dotnet test /nowarn:MSB3277,SA1600,CS1591,CS4014,CS1998
 dotnet publish /nowarn:MSB3277,SA1600,CS1591,CS4014,CS1998 -r=win-x64 -c=Release --self-contained=true .\Mergesort_API\Mergesort_API.csproj
