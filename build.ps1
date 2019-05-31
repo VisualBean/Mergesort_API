8460515b2a592693ae0a618e1303a342455e02d4
Alex <alekcarlsen@gmail.com>
2019-05-31 14:46:04 +0200

    finished build script

--- build.ps1
+++ build.ps1
@@ -1,2 +1,3 @@
-dotnet restore
-dotnet publish -r=win-x64 -c=Release --self-contained=true .\Mergesort_API\Mergesort_API.csproj
+dotnet build /nowarn:MSB3277,SA1600,CS1591,CS4014,CS1998
+dotnet test --no-build
+dotnet publish /nowarn:MSB3277,SA1600,CS1591,CS4014,CS1998 -r=win-x64 -c=Release --self-contained=true .\Mergesort_API\Mergesort_API.csproj
